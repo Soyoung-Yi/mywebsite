71e3182a25c9ea181a32ecbb190a66946baeb174
Soyoung-Yi <spacenyi@naver.com>
2022-01-19 17:32:52 +0900

    PostList

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import ListView
+from .models import Post
+app_name = 'blog'
 urlpatterns = [
-    path('', views.index, name="index"),
+    # path('', views.index, name="index"),
+    path('', views.PostList.as_view(), name='index')
+    # path('', ListView.as_view(model=Post))
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+
+
+class PostList(ListView):
+   model = Post
+   def get_queryset(self):
+      return Post.objects.order_by('title')
 
 
 def index(request):
